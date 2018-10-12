065604eb2a74c0f8a098ee93ba7caa6fffa966d2
d.kanai <d.kanai@pastoraldog.com>
2018-10-12 15:02:09 +0900

    move file

--- java_cucumber/src/test/java/gradle/cucumber/VendingMachineStep.java
+++ java_cucumber/src/test/java/gradle/cucumber/VendingMachineStep.java
@@ -4,7 +4,6 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.cucumber.datatable.DataTable;
-
 import static org.junit.Assert.assertEquals;
 
 public class VendingMachineStep {
