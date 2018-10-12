d4313035eea7cad8453098d8a547be8504631855
d.kanai <d.kanai@pastoraldog.com>
2018-10-12 15:48:40 +0900

    refactor step def. change format support by IDE

--- java_cucumber/src/test/java/gradle/cucumber/VendingMachineStep.java
+++ java_cucumber/src/test/java/gradle/cucumber/VendingMachineStep.java
@@ -1,9 +1,13 @@
 package gradle.cucumber;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.cucumber.datatable.DataTable;
+
+import java.util.Optional;
+
 import static org.junit.Assert.assertEquals;
 
 public class VendingMachineStep {
@@ -15,13 +19,13 @@ public class VendingMachineStep {
         vm = new VendingMachine();
     }
 
-    @Given("{string}を{int}個追加する")
-    public void を_個追加する(String name, Integer number) {
+    @And("^\"([^\"]*)\"を(\\d+)個追加する$")
+    public void を個追加する(String name, int number) {
         vm.addDrink(name, number);
     }
 
-    @When("{int}円を入金")
-    public void 円を入金(Integer money) {
+    @When("^(\\d+)円を入金$")
+    public void 円を入金(int money) {
         vm.charge(money);
     }
 
@@ -32,29 +36,27 @@ public class VendingMachineStep {
         });
     }
 
-    @Then("{int}円が入金されている")
-    public void 円が入金されている(Integer money) {
-        assertEquals(money, vm.currentCharge());
-    }
-
     @When("お釣りを返却")
     public void お釣りを返却() {}
 
-    @Then("{int}円が返却される")
+    @Then("^(\\d+)円が入金されている$")
+    public void 円が入金されている(int money) {
+       assertEquals(Optional.ofNullable(money), Optional.ofNullable(vm.currentCharge()));
+    }
+
+    @Then("^(\\d+)円が返却される$")
     public void 円が返却される(int money) {
         assertEquals(money, vm.reset());
     }
 
-    @When("{string}を購入")
-    public void を購入(String name) {
-        vm.buy(name);
-    }
-
-    @Then("{string}の在庫が{int}個")
-    public void の在庫が_個(String name, int number) {
+    @Then("^\"([^\"]*)\"の在庫が(\\d+)個$")
+    public void の在庫が個(String name, int number) {
         assertEquals(number, vm.inventorySizeBy(name));
     }
 
-
+    @When("^\"([^\"]*)\"を購入$")
+    public void を購入(String name) {
+        vm.buy(name);
+    }
 }
 
