3466c1940a99269b803d87711aedec26ce73f16f
d.kanai <d.kanai@pastoraldog.com>
2018-10-26 11:56:45 +0900

    refactor web driver

--- java_cucumber/src/test/java/gradle/cucumber/E2EStep.java
+++ java_cucumber/src/test/java/gradle/cucumber/E2EStep.java
@@ -8,12 +8,10 @@ import static org.testng.Assert.assertEquals;
 
 public class E2EStep {
 
-    private ChromeDriver driver;
+    private ChromeDriver driver = new WebDriverWrapper().getDriver();
 
     @Given("I visit top page")
     public void i_visit_top_page() {
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-        driver = new ChromeDriver();
         driver.get("file:///Users/d.kanai/Desktop/hello.html");
     }
 
---
+++ java_cucumber/src/test/java/gradle/cucumber/WebDriverWrapper.java
@@ -0,0 +1,15 @@
+package gradle.cucumber;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverWrapper {
+    public ChromeDriver driver = new ChromeDriver();
+
+    public WebDriverWrapper() {
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+    }
+
+    public ChromeDriver getDriver() {
+        return driver;
+    }
+}
