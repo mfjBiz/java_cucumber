381784a96d8b493e3697273bd11a05429e2e5861
d.kanai <d.kanai@pastoraldog.com>
2018-10-26 14:57:43 +0900

    setup spring mvc

---
+++ java_cucumber/src/main/java/practice/e2e/config/Config.java
@@ -0,0 +1,11 @@
+package practice.e2e.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({"practice.e2e.controller"})
+public class Config {
+}
